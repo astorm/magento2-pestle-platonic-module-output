77509e60b3b2b470855e2664416f1365ca93701f
Alan Storm <astorm@alanstorm.com>
2017-02-15 09:39:42 -0800

    Calling real .phar with shell script

--- module.bash
+++ module.bash
@@ -1,21 +1,21 @@
 #!/bin/bash
-pestle_dev magento2:generate:module Pulsestorm Pestleform 0.0.1
-pestle_dev generate_crud_model Pulsestorm_Pestleform Thing
-pestle_dev magento2:generate:acl Pulsestorm_Pestleform Pulsestorm_Pestleform::things
-pestle_dev magento2:generate:menu Pulsestorm_Pestleform "" Pulsestorm_Pestleform::things Pulsestorm_Pestleform::things "Pestle Things" pulsestorm_pestleform_things/index/index 10
-pestle_dev magento2:generate:menu Pulsestorm_Pestleform Pulsestorm_Pestleform::things Pulsestorm_Pestleform::things_list Pulsestorm_Pestleform::things "Thing Objects" pulsestorm_pestleform_things/index/index 10
-pestle_dev generate_route Pulsestorm_Pestleform adminhtml pulsestorm_pestleform_things    
-pestle_dev generate_view Pulsestorm_Pestleform adminhtml pulsestorm_pestleform_things_index_index Main content.phtml 1column
-pestle_dev magento2:generate:ui:grid Pulsestorm_Pestleform pulsestorm_pestleform_things 'Pulsestorm\Pestleform\Model\ResourceModel\Thing\Collection' pulsestorm_pestleform_thing_id
-pestle_dev magento2:generate:ui:form Pulsestorm_Pestleform 'Pulsestorm\Pestleform\Model\Thing' Pulsestorm_Pestleform::things
+pestle.phar magento2:generate:module Pulsestorm Pestleform 0.0.1
+pestle.phar generate_crud_model Pulsestorm_Pestleform Thing
+pestle.phar magento2:generate:acl Pulsestorm_Pestleform Pulsestorm_Pestleform::things
+pestle.phar magento2:generate:menu Pulsestorm_Pestleform "" Pulsestorm_Pestleform::things Pulsestorm_Pestleform::things "Pestle Things" pulsestorm_pestleform_things/index/index 10
+pestle.phar magento2:generate:menu Pulsestorm_Pestleform Pulsestorm_Pestleform::things Pulsestorm_Pestleform::things_list Pulsestorm_Pestleform::things "Thing Objects" pulsestorm_pestleform_things/index/index 10
+pestle.phar generate_route Pulsestorm_Pestleform adminhtml pulsestorm_pestleform_things    
+pestle.phar generate_view Pulsestorm_Pestleform adminhtml pulsestorm_pestleform_things_index_index Main content.phtml 1column
+pestle.phar magento2:generate:ui:grid Pulsestorm_Pestleform pulsestorm_pestleform_things 'Pulsestorm\Pestleform\Model\ResourceModel\Thing\Collection' pulsestorm_pestleform_thing_id
+pestle.phar magento2:generate:ui:form Pulsestorm_Pestleform 'Pulsestorm\Pestleform\Model\Thing' Pulsestorm_Pestleform::things
 
 
 #change title in app/code/Pulsestorm/Pestleform/etc/acl.xml
 #Add ACL Rule to app/code/Pulsestorm/Pestleform/Controller/Adminhtml/Index/Index.php
 #Add Grid to module layout handle file app/code/Pulsestorm/Pestleform/view/adminhtml/layout/pulsestorm_pestleform_things_index_index.xml
 #    - <uiComponent name="pulsestorm_pestleform_things"/> 
-#php bin/magento module:enable Pulsestorm_Pestleform
-#php bin/magento setup:upgrade
+php bin/magento module:enable Pulsestorm_Pestleform
+php bin/magento setup:upgrade
 
 echo "Don't forget to"
 echo "    change title in "
